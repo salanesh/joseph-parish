1f2b482247c15b30c2b86a58a44ffafe9f8ff76e
jhanderick <jhanderick123@gmail.com>
2021-03-07 16:55:57 +0800

    donation module

--- staff-view/staff-donations.php
+++ staff-view/staff-donations.php
@@ -3,141 +3,235 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Staff Announcements</title>
+    <title>Manage Donations</title>
     <?php
     include("../shared-html/header-links-loggedin.html");
+    require("../custom-php/connector.php");
     ?>
 </head>
 <body>
+<script src="../custom-js/staff-functions.js"></script>
     <?php
     require("../shared-html/staffnav.html");
     ?>
-
     <div class="container-fluid staff-content">
-        <!-- Search Bar code -->
-        <div class="container-fluid">
-            <form class="form-inline my-2 mx-2">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search Donations" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
+    <div class="container-fluid">
+        <form class="form-inline my-2 mx-2">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search Donations" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+    
+    
+    <div class="container-fluid my-3 mx-auto" style="width: 20rem">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#AddDonationModal">Add Donation</button>
+    </div>
+    <?php
+        if (isset($_GET['success'])){
+            $success = $_GET['success'];
+            echo '<div class="alert alert-success text-center">';
+            echo '<button class="close" data-dismiss="alert">X</button>';
+            echo $success;
+            echo '</div>';
+        }
+    ?>
+    <div class="container-fluid mx-auto" style="width: 70rem">                     
+        <table class="table table-bordered table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Donated by</th>
+                    <th scope="col">Donation Category</th>
+                    <th scope="col">Event Donation</th>
+                    <th scope="col">Donation Amount</th>
+                    <th scope="col">Donation Date</th>
+                    <th scope="col">Donation Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                  
+                <?php
+                    $sql="SELECT * from donations";
+                    $shit=mysqli_query($connection,$sql); 
+                    while($row = mysqli_fetch_assoc($shit)) {
+                       echo "<tr>";
+                       echo "<th>";
+                       echo $row['donationID'];
+                       echo "</th>";
+                       echo "<td>";
+                       echo $row['userID'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo $row['catID'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo $row['eventID'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo $row['donationamount'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo $row['donationdate'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo $row['donationtype'];
+                       echo "</td>";
+                       echo "<td>";
+                       echo "<button class='btn btn-secondary open-editUser' data-toggle='modal' data-id=".$row['userID']." data-target='#editParishionerModal'>Edit</button>";
+                       echo "&nbsp";
+                       echo "<a href='../controllers/parishioners-controller.php?deactID=".$row['userID']."'><button class='btn btn-danger' onclick = 'return confirm(Are you sure?)'>Deactivate</button></a>";
+                       echo "</td>";
+                       echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    </div>
+    
 
-        <div class="container-fluid my-3 mx-auto" style="width: 20rem">
-            <button class="btn btn-primary" data-toggle="modal" data-target="#addDonationModal">Add Donation</button>
-        </div>
+    <!-- Add Employee Modal -->
 
-        <div class="container-fluid mx-auto" style="width: 70rem">
-            <table class="table table-bordered table-hover">
-                <thead class="bg-grey">
-                    <tr>
-                        <th scope="col">Donated By</th>
-                        <th scope="col">Donation Category</th>
-                        <th scope="col">Goods/Amount Donated</th>
-                        <th scope="col">Date Donated</th>
-                        <th scope="col">Donation Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <!-- put back end shit here -->
-                </tbody>
-            </table>
-        </div>
-
-        <div class="container-fluid my-3 mx-auto" style="width: 20rem">
-            <button class="btn btn-primary" data-toggle="modal" data-target="#donationCategoryModal">Add Donation Category</button>
-        </div>
-
-        <div class="container-fluid mx-auto" style="width: 70rem">
-            <table class="table table-bordered table-hover">
-                <thead class="bg-grey">
-                    <tr>
-                        <th scope="col">Category Name</th>
-                        <th scope="col">Category Description</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <!-- put back end shit here -->
-                </tbody>
-            </table>
-        </div>
-        <!-- Add donation modal -->
-        <div class="modal fade" id="addDonationModal" tabindex="-1" role="dialog" aria-labelledby="addDonationLabel" aria-hidden="true">
+    <div class="modal fade" id="AddDonationModal" tabindex="-1" role="dialog" aria-labelledby="AddDonationLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="addDonationLabel">Add Donation</h5>
+                    <h5 class="modal-title" id="AddDonationLabel">Add Donation</h5>
+
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form>
-        
+                    <form action="../controllers/donation-controller.php" method="POST" name="DonationForm" onsubmit="return(staffValidator());">
                         <div class="form-group">
-                            <label for="donor-name">Donor Name(Can be Anonymous):</label>
-                            <div class="input-group" id="donor-name">
-                                <input type="text" class="form-control" placeholder="donated by">
-                            </div>
+                            <label for="donators-name">Donated By:</label>
+                            <input type="name" class="form-control" id="donation-by-input" name="donatedby" >
+                            <div class="input-group" id="userID">
+                                </div>
                         </div>
                         <div class="form-group">
-                            <label for="donation-amount-input">Donation Amount/Goods</label>
-                            <input type="text" class="form-control" placeholder="" id="donation-amount-input">
-                        </div>
-                        <div class="form-group">
-                            <label for="donation-type-selector">Donation Type: </label>
-                            <select class="form-control" id="donation-type-selector">
-                                <option>Cash</option>
-                                <option>In Kind</option>
+                            <label for="service-selector">Donation Category: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM categories";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[catID]'>".$row["catName"]."</option>";
+                                }
+                            ?>
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="donation-category-selector">Donation Category: </label>
-                            <select class="form-control" id="donation-category-selector">
-                                <option>Roof Repair</option>
-                                <option>Relief Goods</option>
+                            <label for="service-selector">Event Donation: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM  events";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[eventID]'>".$row["eventName"]."</option>";
+                                }
+                            ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="donation-amount-input">Donation Amount</label>
+                            <input type="number" class="form-control" id="donation-amount-input" name="donationamount" >
+                        </div>   
+                        <div class="form-group">
+                            <label for="donation-date-input">Donation Date</label>
+                            <input type="date" id="donation-date-input" name="donationdate">
+                            <div class="form-group">
+                            <label for="service-selector">Donation Type: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM  donations";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[donationID]'>".$row["donationType"]."</option>";
+                                }
+                            ?>
                             </select>
                         </div>
                         <div class="container text-center">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary" name="adddonation">Submit</button>
                          </div>
                     </form>
                 </div>
             </div>
         </div>
-        </div>
-        <!-- Donation Category Modal -->
-        <div class="modal fade" id="donationCategoryModal" tabindex="-1" role="dialog" aria-labelledby="donationCategoryLabel" aria-hidden="true">
+    </div>
+
+
+    <!-- edit employees modal -->
+    <div class="modal fade" id="EditDonationModal" tabindex="-1" role="dialog" aria-labelledby="EditDonationsLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="donationCategoryLabel">Add Donation Category</h5>
+                    <h5 class="modal-title" id="EditDonationLabel">Edit Donation</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form>
-        
+                    <form action="../controllers/donation-controller.php" method="POST" name="DonationUpdateForm">
                         <div class="form-group">
-                            <label for="category-name">Donation Category:</label>
-                            <div class="input-group" id="category-name">
-                                <input type="text" class="form-control" placeholder="category">
-                            </div>
+                            <input type="hidden" name="passedID" id="passedID">
                         </div>
                         <div class="form-group">
-                            <label for="donation-category-desc">Donation Category Description</label>
-                            <textarea class="form-control" id="donation-category-desc" rows="2" id="emp-address-input"></textarea>
+                            <label for="donators-name">Donated By:</label>
+                            <div class="input-group" id="userID">
+                                </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="service-selector">Donation Category: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM categories";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[catID]'>".$row["catName"]."</option>";
+                                }
+                            ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="service-selector">Event Donation: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM  events";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[eventID]'>".$row["eventName"]."</option>";
+                                }
+                            ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="donation-amount-input">Donation Amount</label>
+                            <input type="number" class="form-control" id="donation-email-amount-input" name="donationamount" >
+                        </div>   
+                        <div class="form-group">
+                            <label for="donation-date-input">Donation Date</label>
+                            <input type="date" id="donation-date-input" name="donationdate">
+                            <div class="form-group">
+                            <label for="service-selector">Donation Type: </label>
+                            <select class="form-control" id="service-selector" name="rolekey">
+                            <?php
+                                $sql="SELECT * FROM  donations";
+                                $results=mysqli_query($connection,$sql); 
+                                while($row = mysqli_fetch_assoc($results)) {
+                                    echo "<option value='$row[donationID]'>".$row["donationType"]."</option>";
+                                }
+                            ?>
+                            </select>
                         </div>
                         <div class="container text-center">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary" name="editDonation">Submit</button>
                          </div>
                     </form>
                 </div>
             </div>
         </div>
-        </div>
-
     </div>
 </body>
 </html>
