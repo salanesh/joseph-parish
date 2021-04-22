efa9b832d389ffb4fe880dc57c9e85b010146069
Salanesh <jpls9400@gmail.com>
2021-04-22 17:22:49 +0800

    changed connector stuff

--- custom-php/connector.php
+++ custom-php/connector.php
@@ -1,14 +1,12 @@
 <?php
 $servername ="localhost";
 $database = "parishdata";
-$username = "root";
-$password = "gtjudk1979";
+$username = "cheesecake";
+$password = "Capstone.123";
 
 $connection = mysqli_connect($servername,$username,$password,$database);
 if($connection===false){
     die("Error: Could not connect.".mysqli_connect_error());
 }
-
-
 
 ?>
